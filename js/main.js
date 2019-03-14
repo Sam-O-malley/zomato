da84e6b2f5c2ed54b47cfb0933cc68576de4cf72
Samuel john O'malley <932400765@bun2-206-14.local>
2019-03-14 15:26:14 +1300

    not much has been achieved

--- js/main.js
+++ js/main.js
@@ -1,33 +1,25 @@
-// $.ajax({
-//     url:'js/config.json',
-//     dataType: 'json',
-//     type: 'GET',
-//     success: function(keys) {
-//         key = key[0].userkey;
-//         console.log("the key has loaded")
-//     },
-//     error: function (error) {
-//         console.log(error);
-//         console.log("its not working")
-//     }
-// }),
 
-
-// function getData() {
+function getData() {
     
 $.ajax({
-        url: 'https://developers.zomato.com/api/v2.1/reviews?res_id=7100538',
-        dataType: 'json',
-        // type: 'GET',
-        async: true,
-        beforeSend: function(xhr){xhr.setRequestHeader('user-key', 
-        '0d4d795b7b6b0e26ad6f107918004ec4');
-    },  // This inserts the api key into the HTTP header
+// url: 'https://developers.zomato.com/api/v2.1/reviews?res_id=7100538',
+ url: 'https://developers.zomato.com/api/v2.1/cuisines?city_id=71',
+   dataType: 'json',
+   type: 'GET',
+   async: true,
+   beforeSend: function(xhr){xhr.setRequestHeader('user-key','0d4d795b7b6b0e26ad6f107918004ec4');
+      },// This inserts the api key into the HTTP header
         success: function(response) 
         { console.log(response) } 
     });
-            // }
-    //     })
-    // };
-
+    }
     
+    $(document).ready(function(){
+      $('#data').click(function(){
+        $.getJSON("demo_ajax_json.js", function(result){
+          $.each(result, function(i, field){
+            $("div").append(field + " ");
+          });
+        });
+      });
+    });
